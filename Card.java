a76ba2b0b64a75f42336b79cf5658501e9faa28a
Ricardo Barbosa <ric3639@gmail.com>
2019-11-06 14:16:21 -0800

    Card.java complete

--- Card.java
+++ Card.java
@@ -2,10 +2,10 @@
  * 
  * Card.java
  * 
- * @author 
- * @version
+ * @author Ricardo Barbosa
+ * @version November 6, 2019
  * 
- * The structures for a playing card 
+ * The structure for the Card class 
 */
 
 public class Card 
@@ -28,6 +28,7 @@ public class Card
         set(value, suit);
     }
 
+    //Overloaded constructor
     public Card()
     {
         this('A', Suit.SPADES);
@@ -49,39 +50,60 @@ public class Card
          return errorFlag;
      }
 
-      //Mutators 
-      public boolean set(char value, Suit suit)
-      {
-          if(isValid(value, suit))
-          {
-              this.value = value;
-              this.suit = suit;
-              errorFlag = false;
-              return true;
-          }
-          erroFlag = true;
-          return false;
-      }
+    //Mutators 
+    public boolean set(char value, Suit suit)
+    {
+        if(isValid(value, suit))
+        {
+            this.value = value;
+            this.suit = suit;
+            errorFlag = false;
+            return true;
+        }
+        erroFlag = true;
+        return false;
+    }
 
-      //Valid card data returned 
-      public String toString()
-      {
-          //if statement with errorFlag as parameter
-          //else return value + 'of' + suit string 
-      }
+    //Valid card data returned 
+    public String toString()
+    {
+        if(errorFlag)
+            return "Invalid Card";
+        return value + "of" + suit;
 
-      //Returns true if all field members are identical, false otherwise 
-      public boolean equals(Card card)
-      {
-          //determine how to check if field members are true 
-      }
-      
-      //Determine validity for the value
-      private boolean isValid(char value, Suit suit)
-      {
-          //valid card values
+    }
 
-          //checks if values are valid 
-      }
+    //Returns true if all field members are identical, false otherwise 
+    public boolean equals(Card card)
+    {
+        if(card == null)
+        return false;
+
+        //comparing member values 
+        if (this.value == card.value)
+        {
+            if (this.suit == card.suit)
+                return true;
+        }
+        return false;
+    }
+    
+    //Determine validity for the value
+    private boolean isValid(char value, Suit suit)
+    {
+        //valid card values 
+        char[] validCardValues =
+        {
+            'A','2','3','4','5','6','7','8','9','T','J','Q','K'
+        };
+
+        //checks if value is in the field of valid values 
+        for (int i = 0; i < validCardValues.length; ++i)
+        {
+            if (value == validCardValues[i])
+            return true;
+        }
+        //invalid value
+        return false;
     }
 }
