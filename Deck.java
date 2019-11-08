5df79f14b68236013beed60482ae92c672140dfe
Lindsey Reynolds <lreynolds@csumb.edu>
2019-11-07 17:05:12 -0800

    Added in headings

--- Deck.java
+++ Deck.java
@@ -1,1 +1,70 @@
-// adding code to test pushing online
+/*
+ * Card.java
+ * 
+ * @author 
+ * @version
+ * 
+ * The structure for a deck of cards
+*/
+
+public class Deck() 
+{
+ 
+   // public static variable for maximum cards in a deck
+   public static final int MAX_CARDS = 312; // 6 x 52
+   
+   // private static variable for master pack of cards
+   private static Card[] masterPack;
+   
+   // private member data
+   private Card[] cards;
+   private int topCard;
+
+   // constructor
+   public Deck(int numPacks) 
+   {
+      
+   }
+   
+   // Overloaded constructor 
+   public Deck() 
+   {
+      
+   }
+   
+   public void init(int numPacks)
+   {
+      
+   }
+   
+   public void shuffle()
+   {
+      
+   }
+   
+   public Card dealCard()
+   {
+      
+   }
+   
+   public Card getTopCard()
+   {
+      
+   }
+   
+   public Card inspectCard(int k)
+   {
+      
+   }
+   
+   private static void allocateMasterPack()
+   {
+      
+   }
+   
+   
+}
+
+
+
+
