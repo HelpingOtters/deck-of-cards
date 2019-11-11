5eba1cd2a0795a0925adeab9c138c8ed1dece90d
Lindsey Reynolds <lreynolds@csumb.edu>
2019-11-10 21:58:30 -0800

    practice with deckhand class

--- src/DecksAndHands.java
+++ src/DecksAndHands.java
@@ -9,54 +9,38 @@ public class DecksAndHands
 {
    public static void main(String[] args)
    {
-      // Variable to hold the number of hands the user inputs
       int numHands = getUserInput();
-
       Deck singleDeck = new Deck();
       
       // Step 1: Create a Hands array to hold all the hand objects
       Hand[] hands = createHands(numHands);
 
       // Step 2: Deal the deck into "numHands" Hands
-      while(singleDeck.getTopCard() > 0) // keep dealing until there are no cards left
-      {
-         // Deal one card to each hand
-         for(int x = 0; x < hands.length; x++)
-         {
-            hands[x].takeCard(singleDeck.dealCard());
-         }
-      }
+      dealCards(singleDeck, hands);
 
       // Step 3: Display all the hands
-      System.out.println("Here are your hands, from an unshuffled deck: ");
+      System.out.println("\nHere are your hands, from an unshuffled deck: ");
       for(int i = 0; i < hands.length; i++)
       {
          System.out.println(hands[i]);
       }
 
       // Step 4: Reset the deck and the hands
+      singleDeck.init(1);
+      // reset each Hand object in the hands array
+      for(int i = 0; i < hands.length; i++)
+         hands[i].resetHand();
 
       // Step 5: Shuffle the deck and deal cards again
+      singleDeck.shuffle();
+      dealCards(singleDeck, hands);
 
       // Step 6: Display all the hands again
-      System.out.println("Here are your hands, from a shuffled deck: ");    
-
-   }
-
-   /*
-   * Private helper method to create an array of Hand objects
-   */
-   private static Hand[] createHands(int numHands) 
-   {
-      // Create a Hand array of length numHands
-      Hand[] hands = new Hand[numHands];
-
-      // Populate the array with Hand object
-      for(int i = 0; i < numHands; i++)
+      System.out.println("\nHere are your hands, from a shuffled deck: ");    
+      for(int i = 0; i < hands.length; i++)
       {
-         hands[i] = new Hand();
+         System.out.println(hands[i]);
       }
-      return hands;
    }
 
    /*
@@ -82,6 +66,31 @@ public class DecksAndHands
 
       keyboard.close();
       return numHands;
+   }
+   
+      /*
+    * Private helper method to create an array of Hand objects
+    */
+    private static Hand[] createHands(int numHands) 
+    {
+       // Create a Hand array of length numHands
+       Hand[] hands = new Hand[numHands];
+ 
+       // Populate the array with Hand object
+       for(int i = 0; i < numHands; i++)
+          hands[i] = new Hand();
+       
+      return hands;
+    }
+
+   private static void dealCards(Deck singleDeck, Hand[] hands) 
+   {
+      while(singleDeck.getTopCard() > 0) // keep dealing until there are no cards left
+      {
+         // Deal one card to each hand
+         for(int x = 0; x < hands.length; x++)
+            hands[x].takeCard(singleDeck.dealCard());
+      }
    }
 
 }
